26767fe232cf8e5fa50eba7af3b45ce58db4dd9c
Bhanu Prakash Mallepoola <sravan8147@gmail.com>
2022-07-02 10:14:54 +0530

    Create DateAndTime.js

---
+++ DateAndTime.js
@@ -0,0 +1,23 @@
+var myDate = new Date();
+
+  var month=new Array();
+  month[0]="Jan";
+  month[1]="Feb";
+  month[2]="Mar";
+  month[3]="Apr";
+  month[4]="May";
+  month[5]="Jun";
+  month[6]="Jul";
+  month[7]="Aug";
+  month[8]="Sep";
+  month[9]="Oct";
+  month[10]="Nov";
+  month[11]="Dec";
+  var hours = myDate.getHours();
+  var minutes = myDate.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ampm;
+console.log(myDate.getDate()+" "+month[myDate.getMonth()]+" "+myDate.getFullYear()+" "+strTime)
